099a21052ecae9ab0d4c3735d90df83d1616ac44
AnaM229 <ana.ifrim@yahoo.com>
2022-04-16 16:27:48 +0300

    Added menu pages

---
+++ JS/buttons.js
@@ -0,0 +1,23 @@
+function home(){
+    window.location.href = "home.html";
+}
+
+function drinks(){
+    window.location.href = "drinks.html";
+}
+
+function breakfast(){
+    window.location.href = "breakfast.html";
+}
+
+function lunch(){
+    window.location.href = "lunch.html";
+}
+
+function dinner(){
+    window.location.href = "dinner.html";
+}
+
+function sweets(){
+    window.location.href = "sweets.html";
+}
